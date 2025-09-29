eedc80dc68fc490f76cf568bb0cf8aa22011b5f0
sumit-kakde <115872015+sumit-kakde@users.noreply.github.com>
2025-09-29 09:08:01 +0530

    Time: 47 ms (39.23%), Space: 75.8 MB (20.8%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ #include <bits/stdc++.h>
+class Solution {
+public:
+   ListNode* mergeTwoSortedLinkedLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val <= list2->val) {
+                temp->next = list1;
+                list1 = list1->next;
+            } else {
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            temp = temp->next;
+        }
+
+        if (list1 != nullptr) temp->next = list1;
+        else temp->next = list2;
+
+        return dummyNode->next;
+    }
+    ListNode* findMiddle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* sortList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        ListNode* middle = findMiddle(head);
+        ListNode* right = middle->next;
+        middle->next = nullptr;
+        ListNode* left = head;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        return mergeTwoSortedLinkedLists(left, right);
+    }
+};
